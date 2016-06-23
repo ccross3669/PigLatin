484774e6884b781da5b01dd2ecd1f466c7029984
ccross3669 <charlesdcrossley@gmail.com>
2016-06-23 06:28:50 -0500

    added associated methods

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PigLatin
@@ -10,19 +11,90 @@ namespace PigLatin
     {
         static void Main(string[] args)
         {
-                      
+
             string line = string.Empty;
-            
+
             while (!line.ToLower().Equals("exit".ToLower()))
             {
-                if (line.ToLower().Equals("exit".ToLower()))
-                    break;
+
                 Console.Write("Enter Line: ");
                 line = Console.ReadLine();
 
+                if (line.ToLower().Equals("exit".ToLower()))
+                    continue;
+
+                string[] words = line.Split(' ');
+                string translatedLine = string.Empty;
+                int wordCount = 0;
+                foreach (string word in words)
+                {
+                    if (wordCount != 0)
+                    {
+                        translatedLine = translatedLine + " ";
+                    }
+                    if (!isWordValid(word))
+                    {
+                        translatedLine = translatedLine + word;
+                        continue;
+                    }
+
+
+                    string outPunct;
+                    string outWord;
+                    StripPunctuation(word, out outPunct, out outWord);
+                    
+
+                    wordCount++;
+                }
+
             }
-            
+
         }
 
+        private static bool isWordValid(string word)
+        {
+            int count = Regex.Matches(word, @"[a-zA-Z]").Count;
+
+            if (count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static bool StripPunctuation(string word, out string outPunct, out string outWord)
+        {
+
+            // Assuming that they have used correct punctuation
+            if (word[word.Length - 1].Equals(','))
+            {
+                outPunct = ",";
+                outWord = word.Replace(",", string.Empty);
+                return true;
+            }
+            else if (word[word.Length - 1].Equals('!'))
+            {
+                outPunct = "!";
+                outWord = word.Replace("!", string.Empty);
+                return true;
+            }
+            else if (word[word.Length - 1].Equals('?'))
+            {
+                outPunct = "?";
+                outWord = word.Replace("?", string.Empty);
+                return true;
+            }
+            else
+            {
+                outPunct = string.Empty;
+                outWord = word;
+                return false;
+            }
+
+
+        }
     }
 }
+
